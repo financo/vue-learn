74cf4527e151c8e71087d42aa6f9b17d9817cd6e
wangzhengyu <wangzhengyu@weli.cn>
2021-09-20 22:24:47 +0800

    Initialize

---
+++ test.js
@@ -0,0 +1,13 @@
+console.log("Hello world!")
+
+var name = "wang";
+var name2 = "li";
+function f(name) {
+  var t = "1234"
+  console.log(name);
+  console.log(name2);
+
+}
+
+name = "zhang";
+f(name);
