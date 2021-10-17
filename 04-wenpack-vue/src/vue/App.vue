9dd5de202bce5450f504d2939e8861c47496dfb6
wangzhengyu03 <wangzhengyu03@meituan.com>
2021-10-17 17:19:48 +0800

    NewFeature: vue best practice

--- 04-wenpack-vue/src/vue/App.vue
+++ 04-wenpack-vue/src/vue/App.vue
@@ -3,12 +3,17 @@
     <h2 class="title">{{message}}</h2>
     <button @click="btnClick">Button</button>
     <h2>{{name}}</h2>
+    <Cpn/>
   </div>
 </template>
 
 <script>
+  import Cpn from './Cpn.vue'
   export default {
     name: "App",
+    components: {
+      Cpn
+    },
     data() {
       return {
         message: "Hello Vue!",
---
+++ 04-wenpack-vue/src/vue/Cpn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>我是cpn组件标题</h2>
+    <p>我是cpm组件内容，哈哈哈哈哈！</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Cpn",
+    data() {
+      return {
+        name: "CPN 组件的 name"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
