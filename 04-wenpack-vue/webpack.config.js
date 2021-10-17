afc9766e91cea8da7a3e764277e9fd2eb7c33c41
wangzhengyu03 <wangzhengyu03@meituan.com>
2021-10-17 16:52:12 +0800

    NewFeature: webpack vue

---
+++ 04-wenpack-vue/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    publicPath: "dist/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, {
+            loader: "css-loader"
+          }, {
+            loader: "less-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // 图片大小大于该值, 使用 file-loader 加载, 小于该值使用 base64
+              limit: 20 * 1024,
+              name: "img/[name].[hash:8].[ext]"
+            },
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["es2015"]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      "vue$": "vue/dist/vue.esm.js"
+    }
+  }
+};
