ca1d1ed3373bbd9fa4338d2e17dbc28031b07fe1
wangzhengyu03 <wangzhengyu03@meituan.com>
2021-10-24 18:05:00 +0800

    NewFeature: tab bar

---
+++ 04-vue-router/tabbar/src/components/MainTabBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="blue">
+      <img slot="item-icon" src="../assets/img/tabbar/cart.png" alt="">
+      <img slot="item-icon-active" src="../assets/img/tabbar/cart_active.png" alt="">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category" activeColor="red">
+      <img slot="item-icon" src="../assets/img/tabbar/cart.png" alt="">
+      <img slot="item-icon-active" src="../assets/img/tabbar/cart_active.png" alt="">
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart" activeColor="green">
+      <img slot="item-icon" src="../assets/img/tabbar/cart.png" alt="">
+      <img slot="item-icon-active" src="../assets/img/tabbar/cart_active.png" alt="">
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile" activeColor="deepPink">
+      <img slot="item-icon" src="../assets/img/tabbar/cart.png" alt="">
+      <img slot="item-icon-active" src="../assets/img/tabbar/cart_active.png" alt="">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from "./tabbar/TabBar";
+  import TabBarItem from "./tabbar/TabBarItem";
+  export default {
+    name: "MainTabBar",
+    components: {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- 04-vue-router/tabbar/src/router/index.js
+++ 04-vue-router/tabbar/src/router/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Home from "../views/home/Home";
+import Category from "../views/category/Category";
+import Cart from "../views/cart/Cart";
+import Profile from "../views/profile/Profile";
+
 
 Vue.use(Router)
 
+
+const routes = [
+  {
+    path: "",
+    redirect: "/home"
+  },
+  {
+    path: "/home",
+    component: Home
+  },
+  {
+    path: "/category",
+    component: Category
+  },
+  {
+    path: "/cart",
+    component: Cart
+  },
+  {
+    path: "/profile",
+    component: Profile
+  }
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes,
+  mode: 'history'
 })
