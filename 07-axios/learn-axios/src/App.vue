c4a70701424fac7070646ebacf03cda28f9cf415
wangzhengyu <zzu_wzy@163.com>
2021-11-09 22:52:12 +0800

    revert

--- 07-axios/learn-axios/src/App.vue
+++ 07-axios/learn-axios/src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="app">
-    <h2>Hello World!</h2>
+    <img src="./assets/logo.png">
+    <hello-world></hello-world>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import HelloWorld from "./components/HelloWorld";
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    }
+  }
 </script>
 
 <style>
-
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
