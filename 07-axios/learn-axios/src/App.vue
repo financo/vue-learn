0489cb2e315e0fdc518fa3ec0a2f2c72c64923d2
wangzhengyu <zzu_wzy@163.com>
2021-11-09 22:28:35 +0800

    NewFeature: axios concurrent

--- 07-axios/learn-axios/src/App.vue
+++ 07-axios/learn-axios/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h2>Hello World!</h2>
   </div>
 </template>
 
--- 07-axios/learn-axios/src/main.js
+++ 07-axios/learn-axios/src/main.js
@@ -11,8 +11,53 @@ new Vue({
   render: h => h(App)
 })
 
-axios({
+// *********************** 1.基本使用 **************************
+// axios({
+//   url: 'http://123.207.32.32:8000/home/multidata',
+//   method: 'GET'
+// }).then(res => {
+//   console.log(res)
+// })
+
+// axios({
+//   url: 'http://123.207.32.32:8000/home/data',
+//   params: {
+//     type: 'pop',
+//     page: 1
+//   }
+// }).then(res => {
+//   console.log(res)
+// })
+
+// axios({
+//   url: 'http://123.207.32.32:8000/home/data?type=sell&page=3',
+// }).then(res => {
+//   console.log(res)
+// })
+
+
+// *********************** 2.并发请求 **************************
+// axios.all([axios({
+//   url: 'http://123.207.32.32:8000/home/multidata'
+// }), axios({
+//   url: 'http://123.207.32.32:8000/home/data',
+//   params: {
+//     type: 'pop',
+//     page: 1
+//   }
+// })]).then(res => {
+//   console.log(res)
+// })
+
+axios.all([axios({
   url: 'http://123.207.32.32:8000/home/multidata'
-}).then(res => {
-  console.log(res)
-})
+}), axios({
+  url: 'http://123.207.32.32:8000/home/data',
+  params: {
+    type: 'pop',
+    page: 1
+  }
+})]).then(axios.spread((res1, res2) => {
+  console.log(res1);
+  console.log(res2);
+}));
